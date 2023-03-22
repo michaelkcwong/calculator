80750d3cc52a94edb0178bca1ef2787792471143
michaelkcwong <wong_michael@rogers.com>
2023-03-21 23:00:56 -0400

    Input the digits

--- calculator.js
+++ calculator.js
@@ -13,3 +13,33 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+  // is equivalent to const target = event.target
+
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
